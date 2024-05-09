f91f1bc1fd7762259fd7e504d6d19b628270b00a
davidcoats <david@getsafetycone.com>
2024-05-09 11:23:06 -0700

    Initial commit.

---
+++ source/D8S.L0002.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace D8S.L0002.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/D8S.L0002/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace D8S.L0002
+{
+	/// <summary>
+	/// Personal, catch-call library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
